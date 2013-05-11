02eddb31399399735985b96fa6522ac4d47fcd96
Petro Salema <p.salema@gentics.com>
2013-05-11 16:55:02 +0200

    Update site design

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-(function () {
+(function ($, Parallaxing, Transformer, Aloha) {
 	'use strict';
 
-	var Aloha = {};
-	window.jQuery.fn.aloha = function () {
-		return this;
-	};
-
-	var $ = window.jQuery;
-	var Transformer = window.Transformer;
 	var operation = null;
 
-	function mousedown_on_element(event) {
+	function mousedown(event) {
 		operation = Transformer.start(
 			'rotate',
 			$(event.target).closest('.lettering>b'),
@@ -18,90 +11,60 @@
 		);
 	}
 
-	function mouseup_on_document(event) {
+	function mouseup(event) {
 		if (operation) {
 			Transformer.end(operation);
 			operation = null;
 		}
 	}
 
-	function mousemove_on_document(event) {
+	function mousemove(event) {
 		if (operation) {
 			Transformer.update(operation, event);
 		}
 	}
 
-	var parallax_factor = 0.2;
-	var window_height = 0;
-	var body_height = 0;
-	var containers = [];
-	var contents = [];
-	var heights = [];
-	var bottoms = [];
-	var offsets = [];
-	var $window = $(window);
-	var $sections = $('.parallax');
+	var offsets = {}; 
+	$('.parallax').each(function (i) {
+		offsets[this.id] = i;
+	});
 
-	function reset_element($element) {
-		return $element.css({
-			height: '',
-			marginTop: '',
-			zIndex: ''
-		});
-	}
+	var SCROLL_TO = /#scrollTo\(([a-z0-9]+)\)/;
 
-	function resize_on_window() {
-		window_height = $window.height();
-		var zIndex = $sections.length;
-
-		$sections.css('position', 'relative').each(function (i) {
-			var $element = reset_element($(this)).css({
-				zIndex: zIndex--
-			});
-
-			containers[i] = $element;
-			contents[i] = reset_element($element.children().first());
-			offsets[i] = $element.offset().top;
-			heights[i] = Math.max(window_height, $element.height());
-			bottoms[i] = offsets[i] + heights[i];
-
-			contents[i].height(heights[i]);
-			containers[i].height(heights[i]);
-		}).css('position', 'fixed');
-
-		body_height = bottoms.length ? bottoms[bottoms.length - 1] : 0;
-
-		$('body').height(body_height);
-
-		scroll_on_document();
-	}
-
-	function scroll_on_document() {
-		var offset = $window.scrollTop();
-		var i;
-		var len = containers.length
-		for (i = 0; i < len; i++) {
-			containers[i].css('height', bottoms[i] - offset);
-			contents[i].css(
-				'margin-top',
-				Math.round((offsets[i] - offset) * parallax_factor)
-			);
+	function scrollTo(hash) {
+		var match = hash.match(SCROLL_TO);
+		var offset = offsets[match ? match[1] : hash.replace('#', '')];
+		if (typeof offset === 'number') {
+			$('html, body').animate({
+				scrollTop: offset * $(window).height()
+			}, 1000);
 		}
 	}
 
 	$(document)
-		.on('mouseup', mouseup_on_document)
-		.on('mousemove', mousemove_on_document)
-		.on('mousedown', '.lettering>b', mousedown_on_element)
-		.on('scroll', scroll_on_document);
+		.on('mouseup', mouseup)
+		.on('mousemove', mousemove)
+		.on('mousedown', '.lettering>b', mousedown)
+		.on('scroll', Parallaxing.scroll);
 
-	var scroll_delay = null;
-	$window.on('resize', function () {
-		if (scroll_delay) {
-			clearTimeout(scroll_delay);
-		}
-		scroll_delay = setTimeout(resize_on_window, 200);
+	$(window).on('resize', Parallaxing.delayed_resize);
+
+	Parallaxing.resize();
+
+	$('a[href^="#scrollTo"]').on('click', function () {
+		scrollTo($(this).attr('href'));
 	});
 
-	resize_on_window();
-}());
+	if (window.location.hash) {
+		scrollTo(window.location.hash);
+	}
+
+	Aloha.jQuery('*[contentEditable=true]').aloha();
+
+	$('body').addClass('vendor' + Transformer.VENDOR_PREFIX);
+}(
+	window.jQuery,
+	window.Parallaxing,
+	window.Transformer,
+	window.Aloha
+));
